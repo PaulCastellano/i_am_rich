1f34f9984c4a7659a3d63c4717fb1b0cf33427a3
egaraz <egaraz@netinfo.md>
2022-08-02 12:52:25 +0300

    first commit

---
+++ I'm Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm Rich
+//
+//  Created by Eugeniu Garaz on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
